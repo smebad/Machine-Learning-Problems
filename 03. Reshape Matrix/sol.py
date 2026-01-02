ae66c8774d6497be7b47b7bd799b8f400ea76133
Ebad <146643721+smebad@users.noreply.github.com>
2026-01-02 22:42:51 +0500

    reshape_matrix function and test cases
    Removed comments and cleaned up code structure.

--- 03. Reshape Matrix/sol.py
+++ 03. Reshape Matrix/sol.py
@@ -1,14 +1,4 @@
 # Reshape Matrix
-# Write a Python function that reshapes a given matrix into a specified shape. if it cant be reshaped return back an empty list [ ]
-
-# Example:
-# Input:
-# a = [[1,2,3,4],[5,6,7,8]], new_shape = (4, 2)
-# Output:
-# [[1, 2], [3, 4], [5, 6], [7, 8]]
-# Reasoning:
-# The given matrix is reshaped from 2x4 to 4x2.
-
 # Solution:
 import numpy as np
 
