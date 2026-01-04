9d8d0d3085a44f03d12d5c9bd9ae570c08a89c3a
Ebad <146643721+smebad@users.noreply.github.com>
2026-01-04 22:28:33 +0500

    scalar multiplication function and tests

--- 05. Scalar Multiplication of a Matrix/sol.py
+++ 05. Scalar Multiplication of a Matrix/sol.py
@@ -1,20 +1,7 @@
 # Scalar Multiplication of a Matrix
-# Write a Python function that multiplies a matrix by a scalar and returns the result.
-
-# Example:
-# Input:
-# matrix = [[1, 2], [3, 4]], scalar = 2
-# Output:
-# [[2, 4], [6, 8]]
-# Reasoning:
-# Each element of the matrix is multiplied by the scalar.
-
-
-
 # Solution:
 def scalar_multiply(matrix: list[list[int|float]], scalar: int|float) -> list[list[int|float]]:
     return [[element * scalar for element in row] for row in matrix]
-
 
 # Test cases
 if __name__ == "__main__":
