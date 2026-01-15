44d0581a2d0b9bf37304b238020974609471f74f
Ebad <146643721+smebad@users.noreply.github.com>
2026-01-15 22:21:56 +0500

    implemented function for Random Shuffle of Dataset problem

--- 15. Random Shuffle of Dataset/sol.py
+++ 15. Random Shuffle of Dataset/sol.py
@@ -1,23 +1,4 @@
 # Random Shuffle of Dataset
-# Write a Python function to perform a random shuffle of the samples in two numpy arrays, X and y, while maintaining the corresponding order between them. The function should have an optional seed parameter for reproducibility.
-
-# Example:
-# Input:
-# X = np.array([[1, 2], 
-#                   [3, 4], 
-#                   [5, 6], 
-#                   [7, 8]])
-#     y = np.array([1, 2, 3, 4])
-# Output:
-# (array([[5, 6],
-#                     [1, 2],
-#                     [7, 8],
-#                     [3, 4]]), 
-#              array([3, 1, 4, 2]))
-# Reasoning:
-# The samples in X and y are shuffled randomly, maintaining the correspondence between the samples in both arrays.
-
-
 # Solution:
 import numpy as np
 
