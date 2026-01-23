b872dcb883057713d101cb579eada1a9457d6c90
Ebad <146643721+smebad@users.noreply.github.com>
2026-01-23 18:14:54 +0500

    ReLU Activation Function

---
+++ 23. Leaky ReLU Activation Function/sol.py
@@ -0,0 +1,30 @@
+# Leaky ReLU Activation Function
+# Write a Python function leaky_relu that implements the Leaky Rectified Linear Unit (Leaky ReLU) activation function. The function should take a float z as input and an optional float alpha, with a default value of 0.01, as the slope for negative inputs. The function should return the value after applying the Leaky ReLU function.
+
+# Example:
+# Input:
+# print(leaky_relu(0)) 
+# print(leaky_relu(1))
+# print(leaky_relu(-1)) 
+# print(leaky_relu(-2, alpha=0.1))
+# Output:
+# 0
+# 1
+# -0.01
+# -0.2
+# Reasoning:
+# For z = 0, the output is 0.
+# For z = 1, the output is 1.
+# For z = -1, the output is -0.01 (0.01 * -1).
+# For z = -2 with alpha = 0.1, the output is -0.2 (0.1 * -2).
+
+
+# Solution:
+def leaky_relu(z: float, alpha: float = 0.01) -> float|int:
+    return z if z > 0 else alpha * z
+
+# Test cases:
+print(leaky_relu(0))        # Output: 0
+print(leaky_relu(1))        # Output: 1
+print(leaky_relu(-1))       # Output: -0.01
+print(leaky_relu(-2, 0.1))  # Output: -0.2
