f250cfc3bf6d7e12f130713f16785ed84622aceb
Ebad <146643721+smebad@users.noreply.github.com>
2026-01-28 22:00:18 +0500

    Implement Gini Impurity Calculation function
    Added a test case to demonstrate the Gini Impurity calculation.

--- 28. Implement Gini Impurity Calculation for a Set of Classes/sol.py
+++ 28. Implement Gini Impurity Calculation for a Set of Classes/sol.py
@@ -1,16 +1,4 @@
 # Implement Gini Impurity Calculation for a Set of Classes
-# Task: Implement Gini Impurity Calculation
-# Your task is to implement a function that calculates the Gini Impurity for a set of classes. Gini impurity is commonly used in decision tree algorithms to measure the impurity or disorder within a node.
-
-# Example:
-# Input:
-# y = [0, 1, 1, 1, 0]
-# print(gini_impurity(y))
-# Output:
-# 0.48
-# Reasoning:
-# The Gini Impurity is calculated as 1 - (p_0^2 + p_1^2), where p_0 and p_1 are the probabilities of each class. In this case, p_0 = 2/5 and p_1 = 3/5, resulting in a Gini Impurity of 0.48.
-
 # Solution:
 import numpy as np
 
